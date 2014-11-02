790b55f1d33098943f66f994c48cbec3751b26d2
Cédric VIET <cedric.viet@etu.univ-orleans.fr>
2014-11-02 15:14:50 +0100

    Initialisation de la structure du projet

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Cedric on 02/11/14.
+ */
+@SuppressWarnings("DefaultFileTemplate")
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
